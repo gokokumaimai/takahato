243ba97eb7d774394fb850cbe81ac889621951a5
gokokumaimai <mai_m_1120_moto@yahoo.co.jp>
2019-08-18 15:37:31 +0900

    Conflict resolution2

--- sketch.js
+++ sketch.js
@@ -2,13 +2,11 @@
 const STEP = 30;
 const R = 50;
 
-let plants = new Plants();
+let creature;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  const p = createVector(windowWidth, windowHeight).div(2); // 画面中央
-  const plant = new Plant(p);
-  plants.add(plant);
+  creature = new Creature();
 }
 
 function draw() {
