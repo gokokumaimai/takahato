3360b486106243eca50155287ad90148b0694fc9
gokokumaimai <mai_m_1120_moto@yahoo.co.jp>
2019-08-18 17:09:42 +0900

    changesize

--- lib.js
+++ lib.js
@@ -1,28 +1,42 @@
 function easing(x) {
-  return 1 - (x - 1) ** 4;
+  return sqrt(1 - (x - 1) ** 2);
+  return x;
+  // return 4 * (x - 0.5) ** 3 + 0.5;
 }
 
 class Creature {
   constructor() {
-    this.p = createVector(windowWidth / 2, windowHeight / 2);
+    // 画面中央
+    this.p = createVector(windowWidth / 5, windowHeight / 2);
     this.path = [];
+    this.wait = [];
   }
-  update() {
-    if (this.path.length === 0 && random() < 0.03) {
-      const q = createVector(random(windowWidth - 2 * R) + R, random(windowHeight - 2 * R) + R);
-      for (let i = 0; i < STEP; i++) {
-        const x = (i + 1) / STEP;
+  move() {
+    if (this.path.length > 10) {
+      // １フレーム移動
+      this.p = this.path.shift();
+      this.wait.push(this.p);
+    } else if (this.wait.length > 0) {
+      // その場で待つ
+      this.wait.shift();
+    } else {
+      // STEPフレームの移動を決める
+      // 等速運動
+      const x = random(R, windowHeight - R);
+      const y = random(R, windowWidth  - R);
+      const q = createVector(x, y);
+      //const STEP = int(this.p.dist(q) / MOVE);
+      for (let i = 5; i <= STEP; i++) {
+        const x = i / STEP;
         this.path.push(p5.Vector.lerp(this.p, q, easing(x)));
       }
     }
   }
-  move() {
-    if (this.path.length > 0) {
-      this.p = this.path.shift();
-    }
-  }
   draw() {
-    circle(this.p.x, this.p.y, 50);
-    line(0, 0, this.p.x, this.p.y);
+    for (const w of this.wait) {
+      circle(w.x, w.y, 10);
+    }
+    fill('#8B0000');
+    circle(this.p.x, this.p.y, 100);
   }
 }
